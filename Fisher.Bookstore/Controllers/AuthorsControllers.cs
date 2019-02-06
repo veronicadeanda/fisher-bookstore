431729c8b5cfdcfb09c207bdcf7b7d31a7783d68
veronicadeanda <46544234+veronicadeanda@users.noreply.github.com>
2019-02-06 12:28:43 -0500

    Add files via upload

---
+++ Fisher.Bookstore/Controllers/AuthorsControllers.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Controllers
+{
+    public class AuthorsController : Controller 
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Featured()
+        {
+            return View();
+        }
+
+    }
+}
