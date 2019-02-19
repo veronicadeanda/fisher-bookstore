d819cccdcc81b8082f1b883451b835d967aa64a0
veronicadeanda <46544234+veronicadeanda@users.noreply.github.com>
2019-02-19 18:25:22 -0500

    lab 5

---
+++ api/Models/Book.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Fisher.Bookstore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
+
+    }
+}
