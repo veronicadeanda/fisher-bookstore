f5b95ba3450c67354396f88459c2c3ddafb054df
veronicadeanda <46544234+veronicadeanda@users.noreply.github.com>
2019-02-19 18:23:46 -0500

    lab 5

--- api/Controllers/BooksControllers.cs
+++ api/Controllers/BooksControllers.cs
@@ -1,47 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Fisher.Bookstore.Models;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace Fisher.Bookstore.Api.Controllers
+
+namespace Fisher.Bookstore.Models
 {
-    [Route("api/books")]
-    [ApiController]
-    public class BooksController : ControllerBase
+    public class BookstoreContext : DbContext
     {
-        private readonly BookstoreContext db;
+        public BookstoreContext(DbContextOptions<BookstoreContext> options)
+            : base(options)
+            {
+            }
 
-        public BooksController(BookstoreContext db)
-        {
-            this.db = db;
-
-            if (this.db.Books.Count() == 0)
-                        {
-                            this.db.Books.Add(new Book()
-                            {
-                                Id = 1,
-                                Title = "Design Patterns",
-                                Author = "Erich Gamma",
-                                ISBN = "978-0201633610"
-                            });
-                            this.db.Books.Add(new Book()
-                            {
-                                Id = 2,
-                                Title = "Continuous Delivery",
-                                Author = "Jez Humble",
-                                ISBN = "978-0321601919"
-                            });
-                            this.db.Books.Add(new Book()
-                            {
-                                Id = 3,
-                                Title = "The DevOps Handbook",
-                                Author = "Gene Kim",
-                                ISBN = "978-1942788003"
-                            });                                                        
-                        }
-                        this.db.SaveChanges();
-        }
+            public DbSet<Book> Books { get; set; }
     }
 }
