914714310ac77c506f20a1779845d57256535d82
veronicadeanda <46544234+veronicadeanda@users.noreply.github.com>
2019-03-26 15:59:09 -0400

    Lab 7

---
+++ api/Migrations/20190326195044_init1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Fisher.Bookstore.Api;
+
+namespace Fisher.Bookstore.Api.Migrations
+{
+    public partial class init1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AuthorId",
+                table: "Authors",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Books",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "Authors",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "Authors");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Authors",
+                newName: "AuthorId");
+        }
+    }
+}
