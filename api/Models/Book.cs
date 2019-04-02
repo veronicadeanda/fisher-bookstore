15358eec791d1e0f0801db1cdc47009deff294da
veronicadeanda <46544234+veronicadeanda@users.noreply.github.com>
2019-04-02 14:19:55 -0400

    Lab 8

--- api/Models/Book.cs
+++ api/Models/Book.cs
@@ -8,12 +8,18 @@ namespace Fisher.Bookstore.Api.Models
 
         public string Title { get; set; }
 
-        public string Author { get; set; }
+        public Author Author { get; set; }
 
         public string ISBN { get; set; }
-        
-        public DateTime publishDate { get; set; }
-        
+
+        public DateTime PublishDate { get; set; }
+
         public string Publisher { get; set; }
+
+        public void ChangePublicationDate(DateTime dateTime)
+        {
+            this.PublishDate = dateTime;
+        }
+
     }
 }
