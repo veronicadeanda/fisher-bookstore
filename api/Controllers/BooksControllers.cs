3d9795bdf59fc02ab82ae96adc24584b24f4c81d
veronicadeanda <46544234+veronicadeanda@users.noreply.github.com>
2019-04-02 12:26:40 -0400

    Lab 7

--- api/Controllers/BooksControllers.cs
+++ api/Controllers/BooksControllers.cs
@@ -2,9 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Fisher.Bookstore.Models;
-using Fisher.Bookstore.Data;
+using Fisher.Bookstore.Api.Models;
+using Fisher.Bookstore.Api.Data;
 using Microsoft.AspNetCore.Mvc;
+using Fisher.Bookstore.Api;
 
 namespace Fisher.Bookstore.Api.Controllers
 {
