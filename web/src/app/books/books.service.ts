d17231646084fdec2b8bb15e39fea1c7dea08ee0
veronicadeanda <46544234+veronicadeanda@users.noreply.github.com>
2019-04-09 19:05:03 -0400

    Lab 7

---
+++ web/src/app/books/books.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Book } from './book';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+  private booksUri = 'https://localhost:5001/api/books';
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error('${operation} failed: ${error.message}');
+      return of(result as T);
+    };
+  }
+
+  constructor(private http: HttpClient) {}
+ 
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.booksUri).pipe(
+      tap(_ => console.log('fetched books')),
+      catchError(this.handleError<Book[]>('getBooks', []))
+    );
+  }
+
+}
