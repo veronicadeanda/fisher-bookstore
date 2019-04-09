8d7e2082953f1b11970f251e2401cf5765fcf3f4
veronicadeanda <46544234+veronicadeanda@users.noreply.github.com>
2019-04-09 18:38:34 -0400

    Lab 7

--- api/Controllers/BooksControllers.cs
+++ api/Controllers/BooksControllers.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Fisher.Bookstore.Api.Models;
-using Fisher.Bookstore.Api.Data;
 using Microsoft.AspNetCore.Mvc;
-using Fisher.Bookstore.Api;
 
 namespace Fisher.Bookstore.Api.Controllers
 {
@@ -18,31 +16,31 @@ namespace Fisher.Bookstore.Api.Controllers
         public BooksController(BookstoreContext db)
         {
            this.db = db;
-     //       if (this.db.Books.Count() == 0)
-     //       {
-     //           this.db.Books.Add(new Book()
-     //           {
-     //               Id = 1,
-     //               Title = "Design Patterns",
-     //               Author = "Erich Gamma",
-     //               ISBN = "978-0201633610"
-     //           });
-     //          this.db.Books.Add(new Book()
-     //           {
-     //               Id = 2,
-     //               Title = "Continuous Delivery",
-     //               Author = "Jez Humble",
-     //               ISBN = "978-0321601919"
-     //           });
-     //           this.db.Books.Add(new Book()
-     //           {
-     //               Id = 3,
-     //               Title = "The DevOps Handbook",
-     //               Author = "Gene Kim",
-     //               ISBN = "978-1942788003"
-     //           });
-     //       }
-     //       this.db.SaveChanges();
+            if (this.db.Books.Count() == 0)
+            {
+                this.db.Books.Add(new Book()
+                {
+                    Id = 1,
+                    Title = "Design Patterns",
+                    Author = "Erich Gamma",
+                    ISBN = "978-0201633610"
+                });
+               this.db.Books.Add(new Book()
+                {
+                    Id = 2,
+                    Title = "Continuous Delivery",
+                    Author = "Jez Humble",
+                    ISBN = "978-0321601919"
+                });
+                this.db.Books.Add(new Book()
+                {
+                    Id = 3,
+                    Title = "The DevOps Handbook",
+                    Author = "Gene Kim",
+                    ISBN = "978-1942788003"
+                });
+            }
+            this.db.SaveChanges();
         }
 
         [HttpGet]
@@ -54,7 +52,7 @@ namespace Fisher.Bookstore.Api.Controllers
         [HttpGet("{id}", Name = "GetBook")]
         public IActionResult GetBook(int id)
         {
-            var book = db.Books.Find(id);
+            var book = db.Books.FirstOrDefault(b => b.Id == id);
 
             if (book == null)
             {
@@ -92,8 +90,8 @@ namespace Fisher.Bookstore.Api.Controllers
                 return NotFound();
             }
 
-            //bookToEdit.Title = book.Title;
-            //bookToEdit.ISBN = book.ISBN;
+            bookToEdit.Title = book.Title;
+            bookToEdit.ISBN = book.ISBN;
 
             this.db.Books.Update(bookToEdit);
             this.db.SaveChanges();
