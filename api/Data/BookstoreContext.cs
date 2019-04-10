0a5c9d4a117e3b71ae3a6437bd8b69945fb4d119
veronicadeanda <46544234+veronicadeanda@users.noreply.github.com>
2019-04-09 21:13:33 -0400

    Lab 10

--- api/Data/BookstoreContext.cs
+++ api/Data/BookstoreContext.cs
@@ -1,14 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using Fisher.Bookstore.Api.Models;
 using Fisher.Bookstore.Api;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Fisher.Bookstore.Api.Data
 {
-    public class BookstoreContext : DbContext
+    public class BookstoreContext : IdentityDbContext<ApplicationUser>
     {
         public BookstoreContext(DbContextOptions<BookstoreContext> options) : base(options)
         {
-
         }
 
         protected override void OnModelCreating(ModelBuilder builder) => base.OnModelCreating(builder);
