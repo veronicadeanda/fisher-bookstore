a17fa0199064aed6c4d2b137405b975802468497
veronicadeanda <46544234+veronicadeanda@users.noreply.github.com>
2019-04-09 20:12:18 -0400

    Lab 8

--- api/Startup.cs
+++ api/Startup.cs
@@ -12,10 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using Fisher.Bookstore.Api.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using Fisher.Bookstore.Api.Data;
 
 namespace Fisher.Bookstore.Api
 {
@@ -31,6 +28,9 @@ namespace Fisher.Bookstore.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<BookstoreContext>(options => options.UseNpgsql(Configuration.GetConnectionString("BookstoreContext")));
+
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy", builder =>
@@ -40,7 +40,7 @@ namespace Fisher.Bookstore.Api
                     .AllowAnyHeader();
                 });
             });
-            services.AddDbContext<BookstoreContext>(options => options.UseInMemoryDatabase("Books"));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
