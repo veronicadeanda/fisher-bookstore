7c6ca5c2b74d7513c6b8a2dec4242f8f00f85be4
veronicadeanda <46544234+veronicadeanda@users.noreply.github.com>
2019-04-09 20:10:40 -0400

    Lab 8

--- api/Models/Book.cs
+++ api/Models/Book.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Fisher.Bookstore.Api.Models
 {
@@ -8,10 +9,12 @@ namespace Fisher.Bookstore.Api.Models
 
         public string Title { get; set; }
 
-        public string Author { get; set; }
+        public Author Author { get; set; }
 
         public string ISBN { get; set; }
 
         public DateTime publicationDate { get; set; }
+
+        public string Publisher { get; set; }
     }
 }
